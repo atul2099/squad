3936de99ebda71404d60dadbd9614c9bd0becac8
atul2099 <atulgandhi2099@gmail.com>
2019-12-03 19:45:30 -0500

    added context features

--- setup_modified.py
+++ setup_modified.py
@@ -53,7 +53,7 @@ def url_to_data_path(url):
 def download(args):
     downloads = [
         # Can add other downloads here (e.g., other word vectors)
-        ('GloVe word vectors', args.glove_url),
+        # ('GloVe word vectors', args.glove_url),
     ]
 
     for name, url in downloads:
