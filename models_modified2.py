39d33688cdff357603363c6175bad543989f6f4c
atul2099 <43052221+atul2099@users.noreply.github.com>
2019-12-04 01:38:10 -0500

    Update models_modified2.py

--- models_modified2.py
+++ models_modified2.py
@@ -38,7 +38,7 @@ class BiDAF(nn.Module):
                                     char_channel_width=char_channel_width,
                                     drop_prob=drop_prob)
 
-        self.enc = layers.TransformerEncoder(max_len = 400, dim = hidden_size,output_dim = 2*hidden_size, num_layers = 2,nhead=2)
+        self.enc = layers.TransformerEncoder(max_len = 400, dim = hidden_size,output_dim = 2*hidden_size, num_layers = 1,nhead=2)
 
         # self.enc = layers.RNNEncoder(input_size=hidden_size,
         #                              hidden_size=hidden_size,
@@ -53,7 +53,7 @@ class BiDAF(nn.Module):
         #                              num_layers=2,
         #                              drop_prob=drop_prob)
 
-        self.mod = layers.TransformerEncoder(max_len = 400, dim = 8*hidden_size, output_dim = 2*hidden_size,num_layers = 4,nhead=2)
+        self.mod = layers.TransformerEncoder(max_len = 400, dim = 8*hidden_size, output_dim = 2*hidden_size,num_layers = 2,nhead=2)
 
         self.out = layers.BiDAFOutput(hidden_size=hidden_size,
                                       drop_prob=drop_prob)
